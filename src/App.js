b72a8150b45c1b2628bc5651940610b3417973a8
Raphael Câmara <33887019+Bavaria93@users.noreply.github.com>
2025-02-20 12:29:52 -0300

    Mensagem "Hello world"

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #282c34;
+`;
+
+const Title = styled.h1`
+  font-size: 4rem;
+  color: #61dafb;
+  text-shadow: 2px 2px #000;
+  font-family: 'Arial', sans-serif;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Title>Hello world</Title>
+    </Container>
   );
 }
 
