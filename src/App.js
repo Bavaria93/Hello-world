36cf9961bbdb6f0bc17243cf5ebe9b180d5c7c0e
Raphael Câmara <raphaelbavaria@gmail.com>
2025-02-20 17:29:07 -0300

    api

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-import styled from 'styled-components';
-
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  background-color: #282c34;
-`;
-
-const Title = styled.h1`
-  font-size: 4rem;
-  color: #61dafb;
-  text-shadow: 2px 2px #000;
-  font-family: 'Arial', sans-serif;
-`;
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/data')
+      .then(response => setData(response.data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
   return (
-    <Container>
-      <Title>Hello world</Title>
-    </Container>
+    <div className="App">
+      <h1>React with Flask</h1>
+      {data ? <p>{data.message}</p> : <p>Loading...</p>}
+    </div>
   );
 }
 
